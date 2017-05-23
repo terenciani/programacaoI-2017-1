cedc9ac4734564a77d883cfe8f3863dbcec0c2ee
Marcelo <Marcelo@nt-dell>
2017-05-23 19:44:42 -0400

    Herança inicio

---
+++ src/main/java/br/estacio/terenciani/programacao/aula11/TestePessoa.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.estacio.terenciani.programacao.aula11;
+
+/**
+ *
+ * @author Marcelo
+ */
+public class TestePessoa {
+    public static void  main(String[] args){
+    
+    }
+}
